eabbfecaa1e1e992d60233a52933a608091b0670
Alexander-Koutrakis <61294700+Alexander-Koutrakis@users.noreply.github.com>
2022-09-05 13:48:07 +0300

    9/5

--- Assets/Scripts/Pathfinding/PathfinderMediator.cs
+++ Assets/Scripts/Pathfinding/PathfinderMediator.cs
@@ -10,10 +10,7 @@ namespace Pathfinding
     {
         private Queue<KeyValuePair<Navigator, Vector2>> pathRequests = new Queue<KeyValuePair<Navigator, Vector2>>();
         private Graph graph;
-        private bool calculatingPath = false;
-        private bool answeringRequests = false;
         public static PathfinderMediator Instance { private set; get;}
-        private Thread pathfindingThread;
 
         private Stopwatch sw=new Stopwatch();
 
@@ -21,96 +18,26 @@ namespace Pathfinding
         {
             this.graph = graph;
             Instance = this;
-            pathfindingThread = new Thread(new ThreadStart(AnswerRequests2));
         }
 
         public void PathResquest(Navigator navigator, Vector2 target)
         {
-            KeyValuePair<Navigator, Vector2> request = new KeyValuePair<Navigator, Vector2>(navigator,target);
-            if (!pathRequests.Contains(request))
-            {
-                pathRequests.Enqueue(request);
-                if (!answeringRequests)
-                {
-                    AnswerRequests2();
-                }
-            }
+            Vector2 navigatorPosition = navigator.transform.position;
+            Task.Factory.StartNew(() => SendPath(navigator, target, navigatorPosition));
         }
-        private async Task SendPath(Navigator navigator,Vector2 targetPosition)
+  
+
+        private void SendPath(Navigator navigator, Vector2 targetPosition,Vector2 navigatorPosition)
         {
-            Stopwatch spSW = new Stopwatch();
-            spSW.Start();
-
-            calculatingPath = true;           
-            Vector2 position = navigator.Transform.position;
-            Path path = await Task.Run(() => Pathfinder.GetPath(position, targetPosition, graph, navigator.Transform));            
-            navigator.SetPath(path);
-            calculatingPath = false;
-
-            spSW.Stop();
-            TimeSpan timeSpan = spSW.Elapsed;
-            UnityEngine.Debug.Log("Get path for Navigator "+navigator.name+" is " + timeSpan.TotalMilliseconds);
-           
-
-        }
-
-        private void Sendpath(Navigator navigator, Vector2 targetPosition,Vector2 navigatorPosition)
-        {
-            Stopwatch spSW = new Stopwatch();
-            spSW.Start();
-
-            calculatingPath = true;
-
+            Stopwatch newSW = new Stopwatch();
+            newSW.Start();
             Path path = Pathfinder.GetPath(navigatorPosition, targetPosition, graph, navigator.Transform);
             navigator.SetPath(path);
 
-            spSW.Stop();
-            TimeSpan timeSpan = spSW.Elapsed;
-            UnityEngine.Debug.Log("Timing " + timeSpan.TotalMilliseconds);
-        }
-
-        private async void AnswerRequests2()
-        {
-            answeringRequests = true;
-            while (pathRequests.Count > 0)
-            {
-                if (!pathfindingThread.IsAlive|| pathfindingThread==null)
-                {
-                    KeyValuePair<Navigator, Vector2> navigatorTargetPair = pathRequests.Dequeue();
-                    Navigator navigator = navigatorTargetPair.Key;
-                    Vector2 targetPosition = navigatorTargetPair.Value;
-                    Vector2 navigatorPosition = navigator.transform.position;
-                    pathfindingThread = new Thread(() => Sendpath(navigator, targetPosition, navigatorPosition));
-                    pathfindingThread.Start();
-                }
-                await Task.Yield();
-            }
-            answeringRequests = false;
-        }
-
-        private async void AnswerRequests()
-        {
-            if (!answeringRequests)
-            {
-                sw.Start();
-            }
-            answeringRequests = true;
-            while (pathRequests.Count > 0)
-            {
-
-                if (!calculatingPath)
-                {
-                    KeyValuePair<Navigator, Vector2> navigatorTargetPair = pathRequests.Dequeue();
-                    Navigator navigator = navigatorTargetPair.Key;
-                    Vector2 targetPosition = navigatorTargetPair.Value;
-                    await SendPath(navigator, targetPosition);
-                }
-                await Task.Yield();
-            }
-            answeringRequests = false;
-            sw.Stop();
-            TimeSpan timeSpan = sw.Elapsed;
-            UnityEngine.Debug.Log("Anser Request timing "+timeSpan.TotalMilliseconds);
-        }
+            newSW.Stop();
+            TimeSpan ts = newSW.Elapsed;
+            UnityEngine.Debug.Log("Pathfind " + ts.TotalMilliseconds);
+        }    
+       
     }
 }
